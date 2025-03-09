2962e26db55512c3b56c2631dbf79ddd8ed09e6f
Theodore Chang <tlcfem@gmail.com>
2025-03-09 08:29:24 +0100

    Enable MPI support in VSCode settings and add random PCGBSV test case

--- tests/test.pgbsv.cpp
+++ tests/test.pgbsv.cpp
@@ -120,14 +120,62 @@ void random_psgbsv() {
 }
 
 #ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PCGBSV", "[Simple Solver]") {
+#else
+void random_pcgbsv() {
+#endif
+    const auto& env = get_env<int>();
+
+    const auto context = blacs_context<int>();
+
+    for(auto K = 0; K < 1; ++K) {
+        auto seed = context.amn(static_cast<int>(duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count()));
+        seed = -1190473480;
+        printf("seed: %d\n", seed);
+        std::mt19937 gen(seed);
+
+        auto band = std::uniform_int_distribution(1, 4);
+        const auto KL = band(gen);
+        const auto KU = band(gen);
+        const auto NRHS = band(gen);
+        const auto N = std::uniform_int_distribution(std::max(KL, KU) + 1, 10)(gen);
+
+        const auto LDA = 2 * (KL + KU) + 1;
+
+        const auto IDX = par_cgbsv<int>::indexer{N, KL, KU};
+
+        std::vector<complex8> A, B;
+
+        if(0 == env.rank()) {
+            A.resize(N * LDA, 0.);
+            B.resize(N * NRHS, 1.f);
+
+            std::uniform_real_distribution dist_v(0.f, 1.f);
+
+            for(auto I = 0; I < N; ++I) A[IDX(I, I)] = 10.f * dist_v(gen) + 10.f;
+
+            // std::uniform_int_distribution dist_idx(0, N - 1);
+            //
+            // for(auto I = 0; I < N * N; ++I)
+            //     if(const auto position = IDX(dist_idx(gen), dist_idx(gen)); position >= 0) A[position] += dist_v(gen);
+        }
+
+        const auto info = par_cgbsv<int>().solve({N, N, KL, KU, A.data()}, {N, NRHS, B.data()});
+
+        if(0 == env.rank()) REQUIRE(info == 0);
+    }
+}
+
+#ifdef EZP_ENABLE_TEST
 #else
 int main(const int argc, const char*[]) {
     volatile int i = 0;
     if(argc <= 1)
         while(0 == i) std::this_thread::sleep_for(seconds(10));
 
-    random_pdgbsv();
-    random_psgbsv();
+    // random_pdgbsv();
+    // random_psgbsv();
+    random_pcgbsv();
 
     return 0;
 }
