dfca29df7ba5ef8b53a22a700a7bec91f5dbbb81
Theodore Chang <tlcfem@gmail.com>
2025-03-14 17:22:07 +0100

    Remove old pgesvx test and replace with a new comprehensive test suite

--- tests/test.pgesvx.cpp
+++ tests/test.pgesvx.cpp
@@ -23,9 +23,15 @@
 using namespace ezp;
 using namespace std::chrono;
 
+#ifdef EZP_ENABLE_TEST
+#include <catch2/catchy.hpp>
+#else
+#define REQUIRE(...)
+#endif
+
 static auto N = 10;
 
-template<data_t DT, char FCT = 'E', char ODER = 'R'> auto random_pgesvx() {
+template<data_t DT, char FCT = 'N', char ODER = 'R'> auto random_pgesvx() {
     using solver_t = pgesvx<DT, int_t, FCT, ODER>;
 
     const auto& env = get_env<int_t>();
@@ -37,7 +43,7 @@ template<data_t DT, char FCT = 'E', char ODER = 'R'> auto random_pgesvx() {
         std::mt19937 gen(seed);
 
         const auto NRHS = std::uniform_int_distribution(1, 20)(gen);
-        const auto N = std::uniform_int_distribution(2, 300)(gen);
+        const auto N = std::uniform_int_distribution(100, 400)(gen);
 
         printf("Seed: %d, N: %d, NRHS: %d\n", seed, N, NRHS);
 
@@ -51,23 +57,96 @@ template<data_t DT, char FCT = 'E', char ODER = 'R'> auto random_pgesvx() {
 
             std::uniform_real_distribution dist_v(0.f, 1.f);
 
-            for(auto I = 0; I < N; ++I) {
-                A[IDX(I, I)] = 10.f * dist_v(gen);
-                for(auto J = I + 1; J < std::min(N, I + 2); ++J) A[IDX(I, J)] = dist_v(gen);
-            }
+            for(auto I = 0; I < N; ++I)
+                for(auto J = I; J < std::min(N, I + 2); ++J) A[IDX(I, J)] = dist_v(gen);
         }
 
-        [[maybe_unused]] const auto info = solver_t().solve({N, N, A.data()}, {N, NRHS, B.data()});
+        const auto info = solver_t().solve({N, N, A.data()}, {N, NRHS, B.data()});
+
+        if(0 == env.rank()) REQUIRE(info == 0);
     }
 }
 
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PDGESVX", "[Expert Solver]") {
+#else
+void random_pdgesvx() {
+#endif
+    random_pgesvx<double>();
+}
+
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PDGESVXC", "[Expert Solver]") {
+#else
+void random_pdgesvx_c() {
+#endif
+    random_pgesvx<double, 'N', 'C'>();
+}
+
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PSGESVX", "[Expert Solver]") {
+#else
+void random_psgesvx() {
+#endif
+    random_pgesvx<float>();
+}
+
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PSGESVXC", "[Expert Solver]") {
+#else
+void random_psgesvx_c() {
+#endif
+    random_pgesvx<double, 'N', 'C'>();
+}
+
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PZGESVX", "[Expert Solver]") {
+#else
+void random_pzgesvx() {
+#endif
+    random_pgesvx<complex16>();
+}
+
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PZGESVXC", "[Expert Solver]") {
+#else
+void random_pzgesvx_c() {
+#endif
+    random_pgesvx<complex16, 'N', 'C'>();
+}
+
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PCGESVX", "[Expert Solver]") {
+#else
+void random_pcgesvx() {
+#endif
+    random_pgesvx<complex8>();
+}
+
+#ifdef EZP_ENABLE_TEST
+TEST_CASE("Random PCGESVXC", "[Expert Solver]") {
+#else
+void random_pcgesvx_c() {
+#endif
+    random_pgesvx<complex8, 'N', 'C'>();
+}
+
+#ifndef EZP_ENABLE_TEST
 int main(const int argc, const char* argv[]) {
     volatile int i = 0;
     if(argc <= 1)
         while(0 == i) std::this_thread::sleep_for(seconds(10));
     else N = std::atoi(argv[1]);
 
-    random_pgesvx<double>();
+    random_pdgesvx();
+    random_pdgesvx_c();
+    random_psgesvx();
+    random_psgesvx_c();
+    random_pzgesvx();
+    random_pzgesvx_c();
+    random_pcgesvx();
+    random_pcgesvx_c();
 
     return 0;
 }
+#endif
