cddecf454f5462764783639ae0306aa9736509e7
Theodore Chang <tlcfem@gmail.com>
2025-03-26 21:06:43 +0100

    Add standalone `lis` solver and runner implementation for solving linear equations

---
+++ standalone/runner.lis.cpp
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (C) 2025 Theodore Chang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include <mpl/mpl.hpp>
+
+int main(int, char**) {
+    constexpr int NUM_NODE = 2;
+
+    const auto& comm_world{mpl::environment::comm_world()};
+    const auto worker = comm_world.spawn(0, NUM_NODE, {"solver.lis"});
+    const auto all = mpl::communicator(worker, mpl::communicator::order_low);
+
+    int config[4]{};
+
+    std::string option = "-print all -p ilu -ilu_fill 1 -i fgmres"; // option string
+
+    constexpr int N = 14, NRHS = 1;
+
+    config[0] = option.size(); // length of the option string
+    config[1] = N;             // n
+    config[2] = N;             // nnz
+    config[3] = NRHS;          // nrhs
+
+    std::vector<int> ia(N + 1), ja(N);
+    std::vector<double> a(N), b(N * NRHS, 1.);
+
+    for(auto i = 0; i < N; i++) {
+        ia[i] = ja[i] = i;
+        a[i] = i + 1;
+    }
+    ia[N] = N;
+
+    all.bcast(0, config);
+    all.bcast(0, option.data(), mpl::contiguous_layout<char>(config[0]));
+
+    mpl::irequest_pool requests;
+
+    requests.push(worker.isend(ia, 0, mpl::tag_t{0}));
+    requests.push(worker.isend(ja, 0, mpl::tag_t{1}));
+    requests.push(worker.isend(a, 0, mpl::tag_t{2}));
+    requests.push(worker.isend(b, 0, mpl::tag_t{3}));
+
+    requests.waitall();
+
+    int error = -1;
+    worker.recv(error, 0);
+    if(0 == error) worker.recv(b, 0);
+
+    for(decltype(b.size()) i = 0; i < b.size(); i++) printf("x[%lu] = %+.8f\n", i, b[i]);
+
+    return 0;
+}
---
+++ standalone/solver.lis.cpp
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (C) 2025 Theodore Chang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/**
+ * @brief Standalone `lis` solver.
+ *
+ * This program is a standalone application that solves a system of linear equations
+ * using the `lis` solver.
+ *
+ * The caller spawns this program as a worker process.
+ *
+ * The matrix `A` is stored in the compressed sparse row (CSR) format.
+ * The matrix `A` uses the zero-based indexing.
+ * The caller must send six buffers to the worker process:
+ * - an integer array of length 4, containing:
+ *   - the length of the option string,
+ *   - the number of rows of matrix `A`,
+ *   - the number of non-zero elements of matrix `A`,
+ *   - the number of right-hand sides,
+ * - an option string,
+ * - a buffer containing the row index of matrix `A`, size `NNZ`,
+ * - a buffer containing the column index of matrix `A`, size `NNZ`,
+ * - a buffer containing the value of matrix `A`, size `NNZ`,
+ * - a buffer containing the right-hand side `B`, size `N x NRHS`.
+ *
+ * The error code (0 for success) will be sent back to the root process of the caller.
+ * If error code is 0, the solution will be sent back as well.
+ *
+ * The example caller logic can be seen as follows.
+ *
+ * @include runner.lis.cpp
+ *
+ * @author tlc
+ * @date 26/03/2025
+ * @version 1.0.0
+ * @file solver.lis.cpp
+ * @{
+ */
+
+#include <ezp/lis.hpp>
+#include <mpl/mpl.hpp>
+
+int main(int, char**) {
+    const auto& comm_world{mpl::environment::comm_world()};
+    const auto& parent = mpl::inter_communicator::parent();
+
+    if(!parent.is_valid()) {
+        printf("This program must be invoked by the host application.\n");
+        return 0;
+    }
+
+    const auto all = mpl::communicator(parent, mpl::communicator::order_high);
+
+    LIS_INT config[4]{};
+    all.bcast(0, config);
+
+    std::string opt(config[0], 'x');
+    all.bcast(0, opt.data(), mpl::contiguous_layout<char>(config[0]));
+
+    const auto n = config[1];
+    const auto nnz = config[2];
+    const auto nrhs = config[3];
+
+    auto solver = ezp::lis(opt.c_str());
+
+    std::vector<LIS_INT> ia, ja;
+    std::vector<LIS_SCALAR> a, b;
+
+    if(0 == comm_world.rank()) {
+        ia.resize(nnz);
+        ja.resize(nnz);
+        a.resize(nnz);
+        b.resize(n * nrhs);
+
+        mpl::irequest_pool requests;
+
+        requests.push(parent.irecv(ia, 0, mpl::tag_t{0}));
+        requests.push(parent.irecv(ja, 0, mpl::tag_t{1}));
+        requests.push(parent.irecv(a, 0, mpl::tag_t{2}));
+        requests.push(parent.irecv(b, 0, mpl::tag_t{3}));
+
+        requests.waitall();
+    }
+
+    const auto error = solver.solve({n, nnz, ia.data(), ja.data(), a.data()}, {n, nrhs, b.data()});
+
+    if(0 == comm_world.rank()) {
+        parent.send(error, 0);
+        if(0 == error) parent.send(b, 0);
+    }
+
+    return 0;
+}
+
+//! @}
