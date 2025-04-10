e69fd0f292ca74293d104f349e25ee97b0426439
Theodore Chang <tlcfem@gmail.com>
2025-04-10 05:11:16 +0200

    Automatic sync with master

--- external/mumps/mumps_c_types.h
+++ external/mumps/mumps_c_types.h
@@ -25,12 +25,6 @@
    test from an external code whether MUMPS_INT is 64bits or not */
 #define MUMPS_INTSIZE32
 
-#ifdef EZP_INT64
-#ifndef MUMPS_INTSIZE64
-#define MUMPS_INTSIZE64
-#endif
-#endif
-
 #ifdef MUMPS_INTSIZE64
 #define MUMPS_INT int64_t
 #else
